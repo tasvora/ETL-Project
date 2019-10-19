39336b53b6f9a34c460cfee332b6bcebaefb8ae8
Taylor Breychak <taylor.breychak@outlook.com>
2019-10-19 13:55:30 -0400

    schema file upload

---
+++ schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE indeed_jobs(
+	position VARCHAR,
+	company VARCHAR,
+	description VARCHAR,
+	city VARCHAR,
+	state VARCHAR
+);
+select * from indeed_jobs
+
+CREATE TABLE glassdoor_jobs(
+	position VARCHAR,
+	company VARCHAR,
+	description VARCHAR,
+	city VARCHAR,
+	state VARCHAR
+);
