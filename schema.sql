c3fe956a43623d8bdb180d32391b95596cf8889c
unknown <maria.wisco@gmail.com>
2019-10-21 15:25:50 -0400

    update to indeed_table to match files

--- schema.sql
+++ schema.sql
@@ -1,11 +1,15 @@
+DROP TABLE IF EXISTS indeed_jobs;
+DROP TABLE IF EXISTS glassdoor_jobs;
+
+
 CREATE TABLE indeed_jobs(
-	position VARCHAR,
-	company VARCHAR,
-	description VARCHAR,
-	city VARCHAR,
-	state VARCHAR
+	company_name VARCHAR,
+	job_title VARCHAR,
+	job_summary VARCHAR,
+	City VARCHAR,
+	State VARCHAR
 );
-select * from indeed_jobs
+select * from indeed_jobs;
 
 CREATE TABLE glassdoor_jobs(
 	position VARCHAR,
